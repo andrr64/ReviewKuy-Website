4a74236785064424ee222484385d8cad069036ec
andreas64 <derzandrr03@gmail.com>
2024-09-26 19:10:11 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser'; // Opsional, digunakan jika menggunakan cookies
 import dotenv from 'dotenv';
+import authRoute from './API/route/user/auth.route.js';
+import securityRoute from './API/route/security/token-verification.route.js';
 
 dotenv.config();
 
@@ -11,17 +13,20 @@ const port = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-// Untuk menggantikan __dirname
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'admin', 'dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'admin', 'dist', 'index.html'));
 });
+
+app.use('/auth', authRoute);
+app.use('/security', securityRoute);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
