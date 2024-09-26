369623f3ffecadd857274b7d941e070ad32b200c
andreas64 <derzandrr03@gmail.com>
2024-09-26 14:57:54 +0700

    penambahan beberapa library baru. server - concurrently - dotenv - mongoose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
+import path from 'path';
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser'; // Opsional, digunakan jika menggunakan cookies
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(cookieParser());
 
+// Untuk menggantikan __dirname
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello from server!');
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
