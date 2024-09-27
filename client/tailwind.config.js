e62aa8ef6a7df093bf5d710cc8c3957f5b929ba8
andreas64 <derzandrr03@gmail.com>
2024-09-27 11:02:39 +0700

    update konfigurasi tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner-login': "url(src/assets/background-image/banner-login.jpg)"
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], 
+      },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }
