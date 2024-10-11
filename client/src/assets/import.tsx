b5d3ff1c70327181c22a1279f1585588a7ee5695
andrr64 <derzandrr03@gmail.com>
2024-10-11 10:45:24 +0700

    menambahkan logo rk, google dan ilustrasi

---
+++ client/src/assets/import.tsx
@@ -0,0 +1,7 @@
+import rk_white_logo from './logo/rk-light.svg';
+import google_logo from './brand-logo/google.svg';
+
+export const RK_WhiteLogo = rk_white_logo;
+
+
+export const GoogleLogo =  google_logo;
