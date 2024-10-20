67ad0f63ffd2d515e86db76b8ad243f0b21b29a3
andrr64 <derzandrr03@gmail.com>
2024-10-20 22:24:31 +0700

    menambahkan MainLayout.tsx

---
+++ client/src/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"; // Impor Outlet
+// import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
+
+export default function MainLayout() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Outlet /> {/* Tempat untuk merender rute anak */}
+      <Footer />
+    </div>
+  );
+}
