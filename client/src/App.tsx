ea78b394d0039b98d7caac0d70f6ec295d3372fd
andrr64 <derzandrr03@gmail.com>
2024-10-21 19:39:17 +0700

    memperbaharui rute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/login";
-import RegisterPage from "./pages/registration";
 import Home from "./pages/home";
 import MainLayout from "./layouts/MainLayout";
 
@@ -8,9 +6,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rute tanpa MainLayout */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
 
         {/* Rute dengan MainLayout */}
         <Route element={<MainLayout />}>
