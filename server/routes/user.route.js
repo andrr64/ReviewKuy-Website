94d906b6c271449e33ede27b32dce9c15412de0e
andrr64 <derzandrr03@gmail.com>
2024-10-24 21:22:56 +0700

    pengubahan jalur API

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,6 @@ import { createUser } from "../controllers/user.controller.js";
 
 const API_User = express.Router();
 
-API_User.post('', createUser);
+API_User.post('/account/create', createUser);
 
 export default API_User;
