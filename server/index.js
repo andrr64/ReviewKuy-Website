653aafa3733ba248b0a6139bd63e11a591a16435
andrr64 <derzandrr03@gmail.com>
2024-10-24 08:34:00 +0700

    implementasi rute /api/user

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser'; // Opsional, digunakan jika menggunakan cookies
 import dotenv from 'dotenv';
-import authRoute from './API/route/user/auth.route.js';
-import securityRoute from './API/route/security/token-verification.route.js';
+import mongoose from 'mongoose';
+import API_User from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,16 +16,32 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.use(express.static(path.join(__dirname, 'admin', 'dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// WEB
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
-app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'admin', 'dist', 'index.html'));
-});
+// app.get('/admin', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'admin', 'dist', 'index.html'));
+// });
 
-app.use('/auth', authRoute);
-app.use('/security', securityRoute);
+// RUTE API
+// app.use('/auth', authRoute);
+// app.use('/security', securityRoute);
+app.use('/api/user', API_User);
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log('-----------------------------');
+    console.log('DB OK');
+    console.log('Successfully connected to database');
+    console.log('-----------------------------');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
