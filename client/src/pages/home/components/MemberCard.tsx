7a6668ae376bdaf81764930cd534502a58463657
andrr64 <derzandrr03@gmail.com>
2024-10-24 09:56:44 +0700

    mengubah warna latar dan padding

--- client/src/pages/home/components/MemberCard.tsx
+++ client/src/pages/home/components/MemberCard.tsx
@@ -1,6 +1,6 @@
 function MemberCard() {
   return (
-    <div className="bg-light-purple py-3 px-4">
+    <div className="cursor-pointer bg-gray-50 hover:bg-light-purple transition-300 p-4">
         <img className="h-72 w-60 object-cover" src="https://media.suara.com/pictures/653x366/2021/06/15/91398-benjamin-netanyahu.jpg" alt="" />
         <p className="mt-3 font-bold">M. Dhafa Mahardika</p>
         <p>Setan</p>
