ba1100a655a13b991e6bb4b21c269f57eef1a9f3
andrr64 <derzandrr03@gmail.com>
2024-10-27 10:47:59 +0700

    pembaharuan rute

--- server/routes/brand.route.js
+++ server/routes/brand.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createBrand, deleteBrandById, getBrandById, getBrands, updateBrand } from "../controllers/brand.controller.js";
+import { verifyAdmin } from '../middleware/auth.js'; // Sesuaikan dengan path yang benar
 
 const brand_route = express.Router();
 
-brand_route.post('/',createBrand);
-brand_route.put('/:brandId', updateBrand);
-brand_route.delete('/:brandId', deleteBrandById);
-brand_route.get('/',getBrands);
-brand_route.get('/:brandId', getBrandById);
+brand_route.post('/', verifyAdmin, createBrand); // Menggunakan middleware untuk createBrand
+brand_route.put('/:brandId', verifyAdmin, updateBrand); // Menggunakan middleware untuk updateBrand
+brand_route.delete('/:brandId', verifyAdmin, deleteBrandById); // Menggunakan middleware untuk deleteBrand
+brand_route.get('/', getBrands); // Tidak perlu middleware untuk get
+brand_route.get('/:brandId', getBrandById); // Tidak perlu middleware untuk get
 
 export default brand_route;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createUser, updateUser } from "../controllers/user.controller.js";
+import { createUser, loginUser, logoutUser, updateUser } from "../controllers/user.controller.js";
+import { verifyAccessToken } from "../middleware/auth.js";
 
 const user_route = express.Router();
 
 user_route.post('/', createUser);
-user_route.put('/:userId', updateUser);
+user_route.put('/:userId',verifyAccessToken, updateUser);
+user_route.post('/login', loginUser);
+user_route.post('/logout', logoutUser);
 
 export default user_route;
