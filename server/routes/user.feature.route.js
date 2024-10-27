529b3a5c67ca2effec3513b002c76fc2a5171ace
andrr64 <derzandrr03@gmail.com>
2024-10-28 00:25:54 +0700

    penambahan parameter 'id'

--- server/routes/user.feature.route.js
+++ server/routes/user.feature.route.js
@@ -3,6 +3,6 @@ import { updateUser } from '../controllers/user.controller.js';
 
 const router =  express.Router();
 
-router.put('/update-account', updateUser);
+router.put('/update-account/:id', updateUser);
 
 export default router;
