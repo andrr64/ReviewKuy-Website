30a77e7876945ebf480bf89cbdc7784775da17be
andrr64 <derzandrr03@gmail.com>
2024-10-28 15:51:52 +0700

    rute get

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProductById, getProducts } from '../controllers/product.controller.js';
+import { getProductByBrand, getProductByCategory, getProductById, getProducts } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
+router.get('/brand/:id', getProductByBrand);
+router.get('/category/:id', getProductByCategory);
 
 export default router;
