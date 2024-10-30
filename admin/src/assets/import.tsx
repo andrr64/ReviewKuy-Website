4c2fff49a1395eec89d864a96f398bf1fbc6f264
andrr64 <derzandrr03@gmail.com>
2024-10-30 22:06:36 +0700

    menambahkan asset-asset

---
+++ admin/src/assets/import.tsx
@@ -0,0 +1,8 @@
+import rk_white_logo from './logo/rk-light.svg';
+import rk_full_logo from './logo/rk-full-logo.svg';
+import google_logo from './brand-logo/google.svg';
+
+export const RK_WhiteLogo = rk_white_logo;
+export const RK_FullLogo = rk_full_logo;
+
+export const GoogleLogo =  google_logo;
