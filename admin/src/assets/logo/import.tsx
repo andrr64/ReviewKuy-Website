0a302d4dbb8bcaf7ee9ac5beb244368f6b87f978
andrr64 <derzandrr03@gmail.com>
2024-10-31 02:09:04 +0700

    pemindahan file dan update logo/import.tsx

---
+++ admin/src/assets/logo/import.tsx
@@ -0,0 +1,7 @@
+import rk_white_logo from './rk-light.svg';
+import rk_full_logo from './rk-full-logo.svg';
+import google_logo from './google.svg';
+
+export const RK_WhiteLogo = rk_white_logo;
+export const RK_FullLogo = rk_full_logo;
+export const BrandGoogle = google_logo;
---
+++ admin/src/assets/lotties/import.ts
@@ -0,0 +1,3 @@
+import emptyAnimation from './empty-product.json';
+
+export const EmptyAnimation = emptyAnimation;
