80bc18e7228890fbda76961ffa410508e0f56b87
andrr64 <derzandrr03@gmail.com>
2024-11-01 02:46:19 +0700

    menambahkan konfigurasi 'pool'

--- server/db.js
+++ server/db.js
@@ -6,6 +6,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  pool: {
+    max: 10, // maksimum koneksi di pool
+    min: 0, // minimum koneksi di pool
+    acquire: 30000, // waktu maksimal dalam ms untuk mendapatkan koneksi
+    idle: 10000 // waktu maksimal dalam ms untuk koneksi tidak digunakan
+  }
 });
 
 export default sequelize;
