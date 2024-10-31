d8175462bc0cad014e27ad0fb6987f013f49eb7b
andrr64 <derzandrr03@gmail.com>
2024-11-01 02:29:27 +0700

    penambahan modal 'loading'

---
+++ admin/src/components/modal/LoadingModal.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoadingModal: React.FC = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-lg p-6 text-center">
+        <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-blue-500 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+        <p className="text-gray-800 mt-4">Mengunggah, silakan tunggu...</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingModal;
