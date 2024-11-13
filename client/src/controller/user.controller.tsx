f1547e24b4eb9cb27bf226755bcaeb7966489f50
andrr64 <derzandrr03@gmail.com>
2024-11-14 03:09:31 +0700

    remove unused file

--- client/src/controller/user.controller.tsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { FieldValues } from "react-hook-form";
-
-export const USER_CTRL_CreateUser = async (data : FieldValues) => {
-    const response = await axios.post('/api/user', data);
-    return response.data;
-}
