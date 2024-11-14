22a9ecba1e524a88d2694b29b412ab6070a2815f
andrr64 <derzandrr03@gmail.com>
2024-11-14 23:56:29 +0700

    rute client

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cookieParser from 'cookie-parser'; // Opsional, digunakan jika menggunakan cookies
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import user_route from './routes/user.route.js';
@@ -9,6 +9,7 @@ import admin_route from './routes/admin.route.js';
 import category_route from './routes/category.route.js';
 import product_route from './routes/product.route.js';
 import { initializeApp } from "firebase/app";
+import path from 'path';
 
 dotenv.config();
 
@@ -24,29 +25,35 @@ const firebaseConfig = {
 
 const app = express();
 export const firebaseApp = initializeApp(firebaseConfig);
+const __dirname = path.resolve();  // Mendefinisikan __dirname untuk ES Modules
 
 const startServer = async () => {
   try {
     await sequelize.authenticate();
     const paksa = false;
     console.log('Database connected successfully');
-    await sequelize.sync({ force: paksa }); // Sinkronisasi model ke database
+    await sequelize.sync({ force: paksa });
     console.log('Database synchronized');
 
     // Middleware
     app.use(cors());
-
-    // Mengatur batas ukuran payload untuk JSON
-    app.use(express.json({ limit: '10mb' })); // Atur batas payload menjadi 10MB
+    app.use(express.json({ limit: '10mb' }));
     app.use(cookieParser());
 
-    // Rute
+    // Routes
     app.use('/api/user', user_route);
     app.use('/api/admin', admin_route);
     app.use('/api/brand', brand_route);
     app.use('/api/category', category_route);
     app.use('/api/product', product_route);
 
+    app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+    // Rute catch-all untuk React Router
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    });
+    
     // Menjalankan server
     app.listen(process.env.SERVER_PORT, () => {
       console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
