94c7644dcc3aff1948a574036111f6649a3d0ab4
andrr64 <derzandrr03@gmail.com>
2024-11-18 05:37:06 +0700

    pembaharuan model

--- client/src/model/brand.model.ts
+++ client/src/model/brand.model.ts
@@ -2,10 +2,12 @@ export class BrandModel {
     id: any;
     name: string;
     logo_url: string;
+    description: string;
 
     constructor(data: any){
         this.id = data.id;
         this.name = data.name;
         this.logo_url= data.logo_url;
+        this.description =data.description;
     }
 }
---
+++ client/src/model/product.specification.model.ts
@@ -0,0 +1,9 @@
+export class ProductSpecificationModel {
+    name: string;
+    value: string;
+
+    constructor(data: any) {
+        this.name = data.name;
+        this.value = data.value;
+    }
+}
